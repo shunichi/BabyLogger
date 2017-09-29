145906de2258fa23ef5b44ed8fdc524e09abade2
Shunichi Ikegami <sike.tm@gmail.com>
2017-09-29 13:53:48 +0900

    Test form

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, Alert, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }        
   render() {
     return (
       <View style={styles.container}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here to translate!"
+          keyboardType="numbers-and-punctuation"
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Button
+          onPress={() => {Alert.alert('You tapped the button!')}}
+          title="Press Me"
+        />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
